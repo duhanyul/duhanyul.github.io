bfc3712b2d9943bd720ef9b13c0f33f53a9bf4e1
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-07-13 14:38:18 -0500

    made modal menu

--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
-$(document).ready(function(){
+// $(document).ready(function(){
+  "use strict"
+  console.log("JS loaded");
+  /************ DROPDOWN MENU **********/
+  var dropDown = $('#dropdown');
+  var menu = $('.menu');
+  var menuItems = $(".menu-content");
+  var close = $('.closeBTN');
+  menu.hide();
+
+  dropDown.click(function(){
+    dropDown.hide();
+    menu.animate({
+      width: "toggle"
+    },800);
+    menuItems.animate({
+      width: "30%"
+    },1000)
   });
+  close.click(function(){
+    menu.animate({
+      width: "toggle"
+    },800);
+    menuItems.animate({
+      width: "0%"
+    },1000)
+    close.animate(1200);
+    dropDown.css("display","inline-block");
+  })
+
+
+
+// });
