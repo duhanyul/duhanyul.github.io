40daf6e495eabe9cf6110050ae5945038cb833e8
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-07-14 09:59:09 -0500

    Added mobile detection

--- js/main.js
+++ js/main.js
@@ -7,29 +7,41 @@
   var menuItems = $(".menu-content");
   var close = $('.closeBTN');
   menu.hide();
+  // CHECK IF ON MOBILE PLATFORM
 
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 
+    console.log("MOBILE");
     dropDown.click(function(){
-      if ($(window).width() > 739) {
-        dropDown.hide();
-        menu.animate({
-          width: "toggle"
-        },800);
-        menuItems.animate({
-          width: "30%"
-        },1000)
-      }else {
-        dropDown.hide();
-        menu.animate({
-          width: "toggle"
-        },800);
-        menuItems.animate({
-          width: "100%"
-        },1000)
-      }
-
+      dropDown.hide();
+      menu.animate({
+        width: "toggle"
+      },850);
+      menuItems.animate({
+        width: "100%"
+      },850);
     });
-    close.click(function(){
+    close.click(closing)
+  }
+  // IF NOT ON MOBILE PLATFORM
+
+  else {
+    dropDown.click(function(){
+      dropDown.hide();
+      menu.animate({
+        width: "toggle"
+      },800);
+      menuItems.animate({
+        width: "30%"
+      },850)
+      close.animate(1200)
+    });
+    close.click(closing)
+  }
+
+  // CLOSING ANIMATION FUNCTION
+
+    function closing(){
       menu.animate({
         width: "toggle"
       },800);
@@ -38,8 +50,7 @@
       },1000)
       close.animate(1200);
       dropDown.css("display","inline-block");
-    })
-
+    }
 
 
 
